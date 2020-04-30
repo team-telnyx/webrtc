6c33d0311a0dec89d5fa4b3e3a86f28a396399ac
Deivid Veloso <veloso.deivid@gmail.com>
2020-04-30 16:54:15 -0300

    removed old verto code

--- src/TelnyxRTC.ts
+++ src/TelnyxRTC.ts
@@ -1,3 +1,3 @@
-import TelnyxRTCClient from './TelnyxRTCClient';
+import TelnyxRTCClient from './TelnyxRTCClient/Verto';
 
-export default class TelnyxRTC extends TelnyxRTCClient {}
+export default class TelnyxRTC extends TelnyxRTCClient { }
--- src/TelnyxRTCClient/index.ts
+++
@@ -1,2 +0,0 @@
-import TelnyxRTCClient from './Verto';
-export default TelnyxRTCClient;
