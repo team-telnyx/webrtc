a44d97b95fb677073e18de24007a4f0101201fe5
Deivid Veloso <veloso.deivid@gmail.com>
2020-07-14 13:56:57 -0300

    feat(WEBRTC-121): Added and configured ESlint and Husky (#27)

--- src/Modules/Verto/index.ts
+++ src/Modules/Verto/index.ts
@@ -1,18 +1,19 @@
-import BrowserSession from './BrowserSession'
-import { SubscribeParams, BroadcastParams, ITelnyxRTCOptions } from './util/interfaces'
-import { CallOptions } from './webrtc/interfaces'
-import { Login } from './messages/Verto'
-import Call from './webrtc/Call'
-import { SwEvent, SESSION_ID } from './util/constants'
-import { trigger } from './services/Handler'
-import { localStorage } from './util/storage'
-import VertoHandler from './webrtc/VertoHandler'
-import { isValidOptions } from './util/helpers'
+import BrowserSession from './BrowserSession';
+import { SubscribeParams, BroadcastParams } from './util/interfaces';
+import { CallOptions } from './webrtc/interfaces';
+import { Login } from './messages/Verto';
+import Call from './webrtc/Call';
+import { SwEvent, SESSION_ID } from './util/constants';
+import { trigger } from './services/Handler';
+import { localStorage } from './util/storage';
+import VertoHandler from './webrtc/VertoHandler';
+import { isValidOptions } from './util/helpers';
 
-export const VERTO_PROTOCOL = 'verto-protocol'
+export const VERTO_PROTOCOL = 'verto-protocol';
 
 export default class Verto extends BrowserSession {
   public relayProtocol: string = VERTO_PROTOCOL
+
   public timeoutErrorCode = -329990 // fake verto timeout error code.
 
   validateOptions() {
@@ -20,46 +21,48 @@ export default class Verto extends BrowserSession {
   }
 
   newCall(options: CallOptions) {
-    const { destinationNumber = null } = options
+    const { destinationNumber = null } = options;
     if (!destinationNumber) {
-      throw new Error('Verto.newCall() error: destinationNumber is required.')
+      throw new Error('Verto.newCall() error: destinationNumber is required.');
     }
-    const call = new Call(this, options)
-    call.invite()
-    return call
+    const call = new Call(this, options);
+    call.invite();
+    return call;
   }
 
   broadcast(params: BroadcastParams) {
-    return this.vertoBroadcast(params)
+    return this.vertoBroadcast(params);
   }
 
   subscribe(params: SubscribeParams) {
-    return this.vertoSubscribe(params)
+    return this.vertoSubscribe(params);
   }
 
   unsubscribe(params: SubscribeParams) {
-    return this.vertoUnsubscribe(params)
+    return this.vertoUnsubscribe(params);
   }
 
   protected async _onSocketOpen() {
-    this._idle = false
-    const { login, password, passwd, login_token, userVariables } = this.options
+    this._idle = false;
+    const {
+      login, password, passwd, login_token, userVariables,
+    } = this.options;
     if (this.sessionid) {
-      const sessidLogin = new Login(undefined, undefined, this.sessionid, undefined)
-      await this.execute(sessidLogin).catch(console.error)
+      const sessidLogin = new Login(undefined, undefined, this.sessionid, undefined);
+      await this.execute(sessidLogin).catch(console.error);
     }
-    const msg = new Login(login, (password || passwd), login_token, this.sessionid, userVariables)
-    const response = await this.execute(msg).catch(this._handleLoginError)
+    const msg = new Login(login, (password || passwd), login_token, this.sessionid, userVariables);
+    const response = await this.execute(msg).catch(this._handleLoginError);
     if (response) {
-      this._autoReconnect = true
-      this.sessionid = response.sessid
-      localStorage.setItem(SESSION_ID, this.sessionid)
-      trigger(SwEvent.Ready, this, this.uuid)
+      this._autoReconnect = true;
+      this.sessionid = response.sessid;
+      localStorage.setItem(SESSION_ID, this.sessionid);
+      trigger(SwEvent.Ready, this, this.uuid);
     }
   }
 
   protected _onSocketMessage(msg: any) {
-    const handler = new VertoHandler(this)
-    handler.handleMessage(msg)
+    const handler = new VertoHandler(this);
+    handler.handleMessage(msg);
   }
 }
