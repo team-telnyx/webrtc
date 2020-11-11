7120c8d3e72df73fb13a217cee5cc69d261ef39c
Sua Yoo <sua@telnyx.com>
2020-11-11 15:19:55 -0800

    switch to single media component

---
+++ packages/react-client/src/Media.tsx
@@ -0,0 +1,70 @@
+import React, {
+  useCallback,
+  useContext,
+  useRef,
+  HTMLAttributes,
+  useEffect,
+} from 'react';
+import TelnyxRTCContext from './TelnyxRTCContext';
+
+interface IProps extends HTMLAttributes<HTMLMediaElement> {
+  type: 'audio' | 'video';
+  stream?: MediaStream;
+  isRemote?: boolean;
+  autoPlay?: boolean;
+  controls?: boolean;
+}
+
+function Media({ type, stream, isRemote, ...props }: IProps) {
+  const mediaRef = useRef<HTMLMediaElement>();
+  const client = useContext(TelnyxRTCContext);
+
+  const mediaMountRef = useCallback((node: HTMLMediaElement) => {
+    mediaRef.current = node;
+
+    console.log('hi?');
+
+    if (client) {
+      console.log('hi');
+      if (isRemote) {
+        client.remoteElement = node;
+      } else {
+        client.localElement = node;
+      }
+    } else {
+      if (process.env.NODE_ENV === 'development') {
+        console.warn(
+          `Could not find Telnyx client. Did you wrap your component with \`TelnyxRTCProvider\`?`
+        );
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (mediaRef.current && stream) {
+      mediaRef.current.srcObject = stream;
+    }
+  }, [stream]);
+
+  return React.createElement(type, {
+    ref: mediaMountRef,
+    ...props,
+  });
+}
+
+export function Audio(props: any) {
+  return React.createElement(Media, {
+    type: 'audio',
+    ...props,
+  });
+}
+
+export function Video(props: any) {
+  return React.createElement(Media, {
+    type: 'video',
+    playsInline: true, // prevents mobile browsers from opening video in fullscreen
+    ...props,
+  });
+}
+
+export default Media;
--- packages/react-client/src/index.tsx
+++ packages/react-client/src/index.tsx
@@ -3,3 +3,4 @@ export { default as TelnyxRTCProvider } from './TelnyxRTCProvider';
 export { default as useTelnyxRTC } from './useTelnyxRTC';
 export { default as useCallbacks } from './useCallbacks';
 export { default as useNotification } from './useNotification';
+export { default as Media, Audio, Video } from './Media';
