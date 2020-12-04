e66733bf3ffc2cbde8780018174cc31d30b0ccd9
Deivid Veloso <veloso.deivid@gmail.com>
2020-12-04 15:59:36 -0300

    remove return void

--- packages/js/src/Modules/Verto/webrtc/BaseCall.ts
+++ packages/js/src/Modules/Verto/webrtc/BaseCall.ts
@@ -156,8 +156,6 @@ export default abstract class BaseCall implements IWebRTCCall {
   /**
    * Starts the process to answer the incoming call.
    *
-   * @return void
-   *
    * @examples
    *
    * ```js
