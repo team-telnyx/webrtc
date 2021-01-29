13f32ad51af7d2f69c36f00ac96b95058396ffd2
Deivid Veloso <veloso.deivid@gmail.com>
2021-01-29 10:52:13 -0300

    upgrade storybook and fix some warnings

--- packages/js/examples/react-audio-widgets/.storybook/main.js
+++ packages/js/examples/react-audio-widgets/.storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../**/*.stories.@(js|mdx)'],
   addons: [
     './auth-addon/register.js',
+    '@storybook/addon-docs',
     '@storybook/addon-controls',
     '@storybook/addon-essentials',
   ],
