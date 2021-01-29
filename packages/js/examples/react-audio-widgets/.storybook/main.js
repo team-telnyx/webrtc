4c48bc6f32a16de133497ba34f3c4bf9f909bd97
Deivid Veloso <veloso.deivid@gmail.com>
2021-01-29 11:17:46 -0300

    add custom webpack on storybook

--- packages/js/examples/react-audio-widgets/.storybook/main.js
+++ packages/js/examples/react-audio-widgets/.storybook/main.js
@@ -1,4 +1,14 @@
+// Workaroung https://github.com/webpack/webpack/issues/6426
 module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    config.optimization = {
+      splitChunks: {
+        name: false,
+      },
+    };
+    config.output.chunkFilename = "[name].js"
+    return config;
+  },
   stories: ['../**/*.stories.@(js|mdx)'],
   addons: [
     './auth-addon/register.js',
