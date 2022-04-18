e5602e8d8de6d95fe69da4744971ba9b86f77a1b
Deivid Veloso <veloso.deivid@gmail.com>
2022-04-18 13:39:10 -0300

    chore: update react-native dependencies (#275)

--- packages/react-native/examples/calling-video-app/jest.config.js
+++ packages/react-native/examples/calling-video-app/jest.config.js
@@ -5,8 +5,4 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy',
   },
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
 };
