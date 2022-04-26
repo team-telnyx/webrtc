4d5a5fd2275aef18ea974d6877e675388175d013
Deivid Veloso <veloso.deivid@gmail.com>
2022-04-26 15:01:31 -0300

    chore: removing react-native folder and files that is deprecated

--- packages/react-native/examples/calling-video-app/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  verbose: true,
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  moduleDirectories: ['node_modules', 'src'],
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-  },
-};
