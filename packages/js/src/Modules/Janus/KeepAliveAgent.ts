56ae7ced68408854e8879c627b8fde2e0d3bc1e0
Haythem Farhat <haythem.farhat.dev@gmail.com>
2024-03-11 22:30:58 +0100

    feat: implement keep-alive-agent for connection maintenance
    * Introduces `KeepAliveAgent` class to maintain connection health.
    * Utilizes `Connection` and `transactionManager` for communication.
    * Sends periodic keep-alive transactions (default 10 seconds) to the server.
    * Monitors connection state and restarts keep-alive on reconnection.
    *  Implements failure handling with a configurable retry mechanism.
    
    This feature ensures the connection remains active and facilitates reliable
    communication.

---
+++ packages/js/src/Modules/Janus/KeepAliveAgent.ts
@@ -0,0 +1,60 @@
+import { Connection } from './Connection';
+import { transactionManager } from './TransactionManager';
+import { ConnectionEvents } from './constants';
+import { KeepAliveTransaction } from './transactions/KeepAlive';
+
+type ConstructorParams = {
+  connection: Connection;
+  gatewaySessionId: number;
+};
+
+const KEEP_ALIVE_INTERVAL = 10 * 1000;
+export default class KeepAliveAgent {
+  private _gatewaySessionId: number;
+  private _failedAttempts: number;
+  private _interval: number;
+  private _connection: Connection;
+
+  constructor({ connection, gatewaySessionId }: ConstructorParams) {
+    this._failedAttempts = 0;
+    this._gatewaySessionId = gatewaySessionId;
+    this._connection = connection;
+    this._connection.addListener(
+      ConnectionEvents.StateChange,
+      this._onConnectionStateChange
+    );
+  }
+
+  private _onTick = async () => {
+    try {
+      await transactionManager.execute(
+        new KeepAliveTransaction(this._gatewaySessionId)
+      );
+      this._failedAttempts = 0;
+    } catch (error) {
+      this._failedAttempts += 1;
+      if (this._failedAttempts > 3) {
+        this.stop();
+      }
+    }
+  };
+
+  public start = () => {
+    this.stop();
+    this._interval = window.setInterval(this._onTick, KEEP_ALIVE_INTERVAL);
+  };
+
+  public stop = () => {
+    if (this._interval != null) {
+      window.clearInterval(this._interval);
+      this._interval = null;
+    }
+  };
+
+  private _onConnectionStateChange = () => {
+    if (!this._connection.isAlive) {
+      return this.stop();
+    }
+    this.start();
+  };
+}
