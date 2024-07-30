5043f65340ec45b0879d1e2e7e16c86b2e25b2b2
Haythem Farhat <haythem.farhat.dev@gmail.com>
2024-07-30 15:06:01 +0100

    feat: get registration state from the gateway

---
+++ packages/js/src/Modules/Verto/services/RegisterAgent.ts
@@ -0,0 +1,39 @@
+import BaseSession from '../BaseSession';
+import { Gateway } from '../messages/verto/Gateway';
+import {
+  deferredPromise,
+  DeferredPromise,
+  getGatewayState,
+} from '../util/helpers';
+import { GatewayStateType } from '../webrtc/constants';
+
+export class RegisterAgent {
+  private session: BaseSession;
+  private gatewayStateTask: DeferredPromise<GatewayStateType | ''>;
+  private pendingRequestId: string | null = null;
+
+  constructor(session: BaseSession) {
+    this.session = session;
+    this.gatewayStateTask = deferredPromise<GatewayStateType>({});
+    this.session.on('telnyx.socket.message', this.onSocketMessage);
+  }
+
+  public onSocketMessage = async (response) => {
+    if (response.id === this.pendingRequestId) {
+      this.gatewayStateTask.resolve(getGatewayState(response));
+    }
+  };
+
+  public getIsRegistered = async () => {
+    const message = new Gateway();
+    this.pendingRequestId = message.request.id;
+    this.gatewayStateTask = deferredPromise<GatewayStateType>({});
+
+    this.session.execute(message);
+    const state = await this.gatewayStateTask.promise;
+    if (!state) {
+      return false;
+    }
+    return [GatewayStateType.REGISTER, GatewayStateType.REGED].includes(state);
+  };
+}
