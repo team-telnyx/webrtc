bb639694375cf40680e820cca35f8f69604eae20
Lucas Rosa <lucas@telnyx.com>
2025-09-29 21:43:41 -0300

    feat: add voice_sdk_id to gateway message payload

--- packages/js/src/Modules/Verto/BrowserSession.ts
+++ packages/js/src/Modules/Verto/BrowserSession.ts
@@ -71,6 +71,10 @@ export default abstract class BrowserSession extends BaseSession {
     return 1000;
   }
 
+  async getIsRegistered(): Promise<boolean> {
+    return super.getIsRegistered();
+  }
+
   /**
    * Creates a new connection for exchanging data with the WebRTC server
    *
